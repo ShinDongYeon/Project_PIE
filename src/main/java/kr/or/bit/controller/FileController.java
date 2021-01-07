c7b72b0063461548729190f108e9b8d97b2eac99
honggom <cavok699@naver.com>
2021-01-07 19:45:05 +0900

    new

--- src/main/java/kr/or/bit/controller/FileController.java
+++ src/main/java/kr/or/bit/controller/FileController.java
@@ -1,7 +1,14 @@
 package kr.or.bit.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 
+import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,30 +16,42 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import kr.or.bit.service.FileService;
+
 
 @Controller
 public class FileController {
+	
+	
+	@Autowired
+	private FileService fileservice;
 	
 	private final String UPLOAD_PATH = "Project_PIE/src/main/webapp/resources/files"; 
 	
 	@ResponseBody
 	@RequestMapping(value = "file.pie", method = RequestMethod.POST)
-	public String uploadFile(@RequestParam("files") ArrayList<MultipartFile> files) {
+	public String uploadFile(@RequestParam("files") ArrayList<MultipartFile> files,
+							 @RequestParam("projectNum") int projectNum) {
 		
+		System.out.println("projectNum : "+projectNum);
 		
-		System.out.println(files);
+		System.out.println("file obj : "+files);
+		
 		for(int i = 0; i < files.size(); i ++) {
 			System.out.println(files.get(i).getOriginalFilename());
 			
 		}
 		
-//		 String filename = file.getName();
-//		 String fileOGName =  file.getOriginalFilename();
-//		 
-//		 System.out.println("파일 og 네임 : "+fileOGName);
-//		 System.out.println("파일 네임 : "+filename);
-//		 System.out.println("파일 객체 : "+file);
+	
+		File targetFile = new File(UPLOAD_PATH.resolve(multipartFile.getOriginalFilename()).toString()); 
+		try { 
+			InputStream fileStream = multipartFile.getInputStream(); 
+			FileUtils.copyInputStreamToFile(fileStream, targetFile); 
+		} catch (IOException e) { 
+			FileUtils.deleteQuietly(targetFile); 
+			log.error("Failed to upload ", e); }
 
+		
 		return "success";
 	}
 }
