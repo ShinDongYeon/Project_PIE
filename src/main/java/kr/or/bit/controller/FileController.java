0dbb7596097a4781bc0cd4c467409900c832ed67
honggom <cavok699@naver.com>
2021-01-08 11:29:25 +0900

    file upload

--- src/main/java/kr/or/bit/controller/FileController.java
+++ src/main/java/kr/or/bit/controller/FileController.java
@@ -1,13 +1,7 @@
 package kr.or.bit.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
-
-import java.io.File;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.ArrayList;
-
-import org.apache.commons.io.FileUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,40 +12,29 @@ import org.springframework.web.multipart.MultipartFile;
 
 import kr.or.bit.service.FileService;
 
-
 @Controller
 public class FileController {
-	
 	
 	@Autowired
 	private FileService fileservice;
 	
-	private final String UPLOAD_PATH = "Project_PIE/src/main/webapp/resources/files"; 
-	
 	@ResponseBody
 	@RequestMapping(value = "file.pie", method = RequestMethod.POST)
 	public String uploadFile(@RequestParam("files") ArrayList<MultipartFile> files,
-							 @RequestParam("projectNum") int projectNum) {
+							 @RequestParam("projectNum") int projectNum) throws IOException {
 		
-		System.out.println("projectNum : "+projectNum);
+		boolean check = fileservice.fileUploadService(files, projectNum);
 		
-		System.out.println("file obj : "+files);
-		
-		for(int i = 0; i < files.size(); i ++) {
-			System.out.println(files.get(i).getOriginalFilename());
-			
+		if(check) {
+			System.out.println("파일 업로드 성공");
+			return "success";
+		}else {
+			System.out.println("파일 업로드 실패");
+			return "fail";
 		}
-		
 	
-		File targetFile = new File(UPLOAD_PATH.resolve(multipartFile.getOriginalFilename()).toString()); 
-		try { 
-			InputStream fileStream = multipartFile.getInputStream(); 
-			FileUtils.copyInputStreamToFile(fileStream, targetFile); 
-		} catch (IOException e) { 
-			FileUtils.deleteQuietly(targetFile); 
-			log.error("Failed to upload ", e); }
+  }
+	
 
-		
-		return "success";
-	}
+
 }
--- src/main/java/kr/or/bit/service/FileService.java
+++ src/main/java/kr/or/bit/service/FileService.java
@@ -1,28 +1,80 @@
 package kr.or.bit.service;
 
-import java.util.HashMap;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import kr.or.bit.dao.ListDao;
-
-/*
-파일명: KanbanService.java
-설명: 칸반 보드에서 리스트와 카드 추가,수정,삭제 및 정렬 작업 후 db에 저장
-작성일: 2020-12-28 ~ 2021-01-07
-작성자: 문지연,변재홍
-*/
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
-public class FileService {
-
+public class FileService{
+	
 	@Autowired
 	private SqlSession sqlsession;
-
-	// 칸반 리스트 업데이트 (한개씩)
-	public boolean updateKanbanList(HashMap<String, Object> listAndProjectNum) {
-		ListDao listdao = sqlsession.getMapper(ListDao.class);
-		listdao.updateKanbanList(listAndProjectNum);
+	
+	//파일 저장 절대 경로 
+	private final String UPLOAD_PATH = "/Users/byeonjaehong/Desktop/project3_final_forked/Project_PIE/src/main/webapp/resources/files"; 
+	
+	//파일 업로드 서비스 
+	public boolean fileUploadService(ArrayList<MultipartFile> files, int projectNum) {
+		
+		//파일 저장 경로 (프로젝트번호 기준)
+		String specific_path = "/file_directory_project_seq_"+projectNum;
+		
+		File fileOb = new File(UPLOAD_PATH+specific_path);
+		
+		//폴더 존재 여부 
+		if(fileOb.isDirectory()) {
+			System.out.println("디렉토리 있음");
+		}else {
+			System.out.println("디렉토리 없음");
+			fileOb.mkdir();
+		}
+		
+		for(int i = 1; i <= (files.size()-1); i ++) {
+			System.out.println("list size : "+files.size());
+			String fileOGName = files.get(i).getOriginalFilename();
+			System.out.println(i+" 번째 파일 OG 명 "+fileOGName);
+		File fileCheck = new File(UPLOAD_PATH+specific_path+"/"+fileOGName);
+		
+		//파일 확장자 
+		String ext = fileOGName.substring(fileOGName.lastIndexOf(".") + 1);
+		
+		//파일 이름 중복 시 
+		if(fileCheck.exists()) {
+			System.out.println("파일 이름 중복");
+			
+			//파일 이름 뒤에 @ 붙여준 후 업로드 진행 
+			String Changed_fileName = fileOGName.substring(0, fileOGName.indexOf("."));
+			fileOGName = Changed_fileName+"@."+ext;
+		}
+	
+		System.out.println("파일 확장자 : "+ext);
+		
+			byte[] data;
+			try {
+				data = files.get(i).getBytes();
+				
+				//절대경로 + 프로젝트번호 + 파일이름 
+				FileOutputStream fos = new FileOutputStream(UPLOAD_PATH+specific_path+"/"+fileOGName);
+				
+				//파일 업로드 
+				fos.write(data);
+				
+				//디비에 파일 정보 저장
+				//원래 이름, 저장된 이름, 프로젝트 번호, 확장자, 저장시간 
+				fos.close();
+			} catch (IOException e) {
+				System.out.println(e.getMessage());
+				e.printStackTrace();
+				return false;
+			}
+		}
 		return true;
 	}
+
+
 }
