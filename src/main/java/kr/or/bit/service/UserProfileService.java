aabb07341a0120b2a1f76c800984cbfffd66ab74
honggom <cavok699@naver.com>
2021-01-14 09:50:57 +0900

    new

--- src/main/java/kr/or/bit/service/UserProfileService.java
+++ src/main/java/kr/or/bit/service/UserProfileService.java
@@ -15,24 +15,19 @@ public class UserProfileService{
 	
 	@Autowired
 	private SqlSession sqlsession;
-	
 
 	//재홍
-	//private final String UPLOAD_PATH = "/Users/byeonjaehong/Desktop/project3_final_forked/Project_PIE/src/main/webapp/resources/profile"; 
+	private final String UPLOAD_PATH = "/Users/byeonjaehong/Desktop/project3_final_forked/Project_PIE/src/main/webapp/resources/profile"; 
 	
-	//파일 저장 절대 경로 
-
-	//재홍
-	//private final String UPLOAD_PATH = "/Users/byeonjaehong/Desktop/project3_final_forked/Project_PIE/src/main/webapp/resources/profile"; 
 	//동연
-	private final String UPLOAD_PATH = "C:\\aaaa\\Project_PIE\\src\\main\\webapp\\resources\\profile";
+	//private final String UPLOAD_PATH = "C:\\aaaa\\Project_PIE\\src\\main\\webapp\\resources\\profile";
 
 	//재구
 	//private final String UPLOAD_PATH = "C:\\develop\\Spring\\PIE_workspace2\\Project_PIE\\src\\main\\webapp\\resources\\profile"; 
-
 	
 	//지연
-	private final String UPLOAD_PATH = "C:\\Users\\jiyeo\\Desktop\\Project_PIE\\src\\main\\webapp\\resources\\profile"; 
+	//private final String UPLOAD_PATH = "C:\\Users\\jiyeo\\Desktop\\Project_PIE\\src\\main\\webapp\\resources\\profile"; 
+	
 	//파일 업로드 서비스 
 	public void profileUploadService(MultipartFile file, String email) {
 		String fileOGName = file.getOriginalFilename();
